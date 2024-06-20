ec977b6254cea2866847ab7017898248020bb2b3
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-06-20 16:50:42 +0300

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,18 @@ import {wasm} from '@rollup/plugin-wasm';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'src/index.js',
-  output: {
-    name: 'mapbox-gl-rtl-text',
-    file: 'dist/mapbox-gl-rtl-text.js',
-    format: 'umd',
-  },
-  plugins: [
-    wasm({targetEnv: 'auto-inline'}),
-    nodeResolve(),
-    terser({
-      ecma: 2020,
-      module: true,
-    })
-  ]
+    input: 'src/index.js',
+    output: {
+        name: 'mapbox-gl-rtl-text',
+        file: 'dist/mapbox-gl-rtl-text.js',
+        format: 'umd',
+    },
+    plugins: [
+        wasm({targetEnv: 'auto-inline'}),
+        nodeResolve(),
+        terser({
+            ecma: 2020,
+            module: true,
+        })
+    ]
 };
