8469ddfd9efa61369aab25f991a179cbed94f5d4
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-07-24 13:31:00 +0300

    Use ICU version from Emscripten

--- build_icu.sh
+++ build_icu.sh
@@ -21,69 +21,15 @@ else
     export HOST_ARG="x86_64-unknown-linux-gnu"
 fi
 
-# Download ICU source code
 mkdir -p build
-wget -q https://github.com/unicode-org/icu/releases/download/release-75-1/icu4c-75_1-src.tgz -O ./build/icu4c.tgz
-tar -xzf build/icu4c.tgz -C build
-
-# Build ICU
-pushd ./build/icu/source
-./configure \
-    --host $HOST_ARG \
-    --enable-tools \
-    --with-data-packaging=archive \
-    --enable-renaming \
-    --enable-strict \
-    --enable-static \
-    --enable-draft \
-    --enable-release \
-    --disable-debug \
-    --disable-rpath \
-    --disable-shared \
-    --disable-tests \
-    --disable-extras \
-    --disable-tracing \
-    --disable-layout \
-    --disable-icuio \
-    --disable-samples \
-    --disable-dyload
-
-make clean
-make -j4
-make install
-
-emconfigure ./configure \
-    --host $HOST_ARG \
-    --with-cross-build=$(pwd) \
-    --with-data-packaging=archive \
-    --enable-renaming \
-    --enable-strict \
-    --enable-static \
-    --enable-draft \
-    --enable-release \
-    --disable-tools \
-    --disable-debug \
-    --disable-rpath \
-    --disable-shared \
-    --disable-tests \
-    --disable-extras \
-    --disable-tracing \
-    --disable-layout \
-    --disable-icuio \
-    --disable-samples \
-    --disable-dyload
-
-emmake make clean
-emmake make -j4
-emmake make install
-popd
 
 # Build ubidi and ushape wrappers
-emcc -Oz -c ./src/ubidi_wrapper.c -o ./build/ubidi_wrapper.o -I ./build/icu/source/common/
-emcc -Oz -c ./src/ushape_wrapper.c -o ./build/ushape_wrapper.o -I ./build/icu/source/common/
+emcc -Oz -s USE_ICU=1 -c ./src/ubidi_wrapper.c -o ./build/ubidi_wrapper.o
+emcc -Oz -s USE_ICU=1 -c ./src/ushape_wrapper.c -o ./build/ushape_wrapper.o
 
 # Compile ICU wrapper to WebAssembly, embed all subresources as base64 string literals and export as a ES module
-emcc -Oz -v -o ./src/icu.wasm.js ./build/ushape_wrapper.o ./build/ubidi_wrapper.o ./build/icu/source/lib/libicuuc.a \
+emcc -Oz -v -o ./src/icu.wasm.js ./build/ushape_wrapper.o ./build/ubidi_wrapper.o \
+    -s USE_ICU=1 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s DEAD_FUNCTIONS="[]" \
     -s ENVIRONMENT="web,worker" \
